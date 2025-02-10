567f3541316b79bc38ca6234f901fd462e08384f
Prince Clark <princeyclarky@gmail.com>
2025-02-10 01:26:23 -0500

    added print function

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("I really really really love computer science");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
